26e554f63d0dfeba8aab14e11bdd680a28432c6f
umalik87 <umalik87@gmail.com>
2019-10-13 04:14:15 -0400

    Handling Express Errors

--- src/index.js
+++ src/index.js
@@ -11,10 +11,29 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    }, 
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word Document'))
+        }
+
+        cb(undefined, true)
+
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    }
 })
+// const errorMiddleware = (req, res, next) => {
+//     throw new Error('From my middleware')
+// }
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 
